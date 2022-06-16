ca2f5fb4929347e99ac13753ee1eda5a2d1cc823
Eduardo Maciel de Matos <mrgamex49@gmail.com>
2022-06-16 13:14:47 -0300

    feat(automated-pr): create automated pr with tag

---
+++ automated-pr-template.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env zx
+
+let tag = process?.argv[3] ?? '';
+let url = process?.argv[4] ?? JSON.parse((await $`gh repo view --json url`).toString()).url; 
+
+let gpIssueListAssign = (await $`gh issue list -a "@me" -R ${url}`)
+                          .toString()
+                          .split('\n')
+                          .map(line => line.split('\t'));
+
+let idOfLastIssueAssigned = gpIssueListAssign[0][0];
+
+// await $`git push -u origin HEAD` // to not require interective on gh pr create 
+let prBody = `"${tag}: ${url}/issues/${idOfLastIssueAssigned}"`;
+let prLink = (await $`echo gh pr create --fill --body ${prBody}`).toString();
+
+// await $`xdg-open "${prLink}"`; // open in browser to review

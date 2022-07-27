4bd54d99b36f511f324afe39cb82d4fb7688e3bd
Eduardo Maciel de Matos <mrgamex49@gmail.com>
2022-07-27 16:18:01 -0300

    feat(batch-issues): be able to add many issues on checklist

--- auto-update-checklist.mjs
+++ auto-update-checklist.mjs
@@ -1,14 +1,31 @@
 #!/usr/bin/env zx
 
 const targetIssueId = process.argv[3] || '';
+
 let body = JSON.parse((await $`gh issue view ${targetIssueId} --json body`).toString());
 
 const issueIdToAppend = process.argv[4] || '';
+console.log({issueIdToAppend});
+const batchId = issueIdToAppend.split(',');
 
-let urlAndTitle = JSON.parse((await $`gh issue view ${issueIdToAppend} --json url,title`).toString());
+if(batchId.length > 1) {
+  await batchId.forEach(async (id) => {
+    let urlAndTitle = JSON.parse((await $`gh issue view ${id} --json url,title`).toString());
 
-const checklistFormat = `- [ ] ${urlAndTitle.title} (${urlAndTitle.url})`;
+    const checklistFormat = `- [ ] ${urlAndTitle.title} (${urlAndTitle.url})`;
+    
+    body.body+= `\n${checklistFormat}`;
+    
+    const newIssueId = (await $`gh issue edit ${targetIssueId} --body ${body.body}`).toString();
+  });
 
-body.body+= `\n${checklistFormat}`;
+} else {
+  let urlAndTitle = JSON.parse((await $`gh issue view ${issueIdToAppend} --json url,title`).toString());
 
-const newIssueId = (await $`gh issue edit ${targetIssueId} --body ${body.body}`).toString();
+  const checklistFormat = `- [ ] ${urlAndTitle.title} (${urlAndTitle.url})`;
+  
+  body.body+= `\n${checklistFormat}`;
+  
+  const newIssueId = (await $`gh issue edit ${targetIssueId} --body ${body.body}`).toString();
+}
+

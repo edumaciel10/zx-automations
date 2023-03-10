46e604809a2cbba8fcc4bd6e8105a01010601d40
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2023-03-10 18:25:23 -0300

    feat(i18n): translate all __STRING_NOT_TRANSLATED__ into .json files (#26)

---
+++ translateI18n.mjs
@@ -0,0 +1,57 @@
+// Define the path to your JSON file and the new value to replace the placeholder string
+const [, , , inputFile] = process.argv;
+
+import { fs } from 'zx';
+const filePath = path.resolve(inputFile);
+
+const language = filePath.split('/').pop().split('.')[0] === 'pt-BR' ? 'pt' : 'es';
+
+const filePaths = [
+  filePath,
+  language === 'pt' ? filePath.replace('pt-BR.json', 'es.json') : filePath.replace('es.json', 'pt-BR.json')
+]
+// read the JSON file
+
+for (const inputFilePath of filePaths ) {
+  const json = await fs.readFile(inputFilePath, 'utf8');
+
+  const parsedData = JSON.parse(json);
+
+  let stringsToParse = [];
+
+  for (const key in parsedData) {
+    // Check if the value of the key is the placeholder string
+    if (parsedData[key] === '__STRING_NOT_TRANSLATED__') {
+      stringsToParse.push({
+        [key]: parsedData[key],
+      });
+    }
+  }
+
+  const promiseParseString = stringsToParse.map(async (value, key) => {
+    const string = Object.keys(value)[0];
+    const translatedString = (await $`trans -brief -s en -t ${language} ${string}`).toString().trim().replace(`\n`, '');
+
+    console.log({string, translatedString})
+    return { [string]: translatedString };
+  })
+
+  const translatedStringsParsed = await Promise.all(promiseParseString).then((value) => value).catch((err) => err);
+
+  console.log({translatedStringsParsed})
+
+  const mappedObject = Object.fromEntries(
+    Object.entries(parsedData).map(([key, value]) => {
+      const translatedString = translatedStringsParsed.find((string) => string[key]);
+      if (translatedString) {
+        return [key, translatedString[key]];
+      }
+      return [key, value];
+    })
+  );
+
+
+  // write into the JSON file
+
+  await fs.writeFile(inputFilePath, JSON.stringify(mappedObject, null, 2));
+}
